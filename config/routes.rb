17332677bced464ba208598fa3e97c9eab0da978
Melony Franchini <meltravelz@gmail.com>
2023-05-23 12:18:50 -0600

    add route for heath? aws

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'api/health_check', to: proc { [200, {}, ['']] }
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :api do
